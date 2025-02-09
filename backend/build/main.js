f4514e9cd0d47d6aefea799b11db5fe4abb17591
SvetaSaletska <svitlanagalevich@gmail.com>
2025-02-10 01:50:57 +0200

    update backend

--- backend/build/main.js
+++
@@ -1,5 +0,0 @@
-import { analytics } from "./modules/analytics.js";
-const message = "Hello world";
-console.log(message);
-analytics("Main.ts!");
-//# sourceMappingURL=main.js.map
--- backend/src/main.ts
+++
@@ -1,5 +0,0 @@
-import { analytics } from './modules/analytics.js';
-const message: string = 'Hello world';
-console.log(message);
-
-analytics('Main.ts!');
